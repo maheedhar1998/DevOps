cae8d93497e3437c8c7d35d71d6c753f0651ea66
maheedhar1998 <maheedhar1998@hotmail.com>
2020-12-19 06:47:41 -0500

    Update added aws-cli-go lang folder and file

---
+++ go_aws_cli_wrapper/aws-cli.go
@@ -0,0 +1,15 @@
+package go_aws_cli_wrapper
+
+import (
+	// "github.com/aws/aws-sdk-go/aws"
+	// "github.com/aws/aws-sdk-go/aws/session"
+	// "github.com/aws/aws-sdk-go/service/s3"
+)
+
+func listS3Buckets() []string {
+	return []string{""}
+}
+
+func createS3Bucket() bool {
+
+}
